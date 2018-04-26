432bb7ddeecd80ff01d81bbfc0c71c1e36ca73f8
Jost Gombac <gombac.jost@gmail.com>
2018-04-26 12:19:23 +0200

    change details display

--- app.js
+++ app.js
@@ -6,54 +6,67 @@ var app = new Vue({
         images:  [
             {
                 name: "bright_dark_contrast",
-                theme: "dark"
+                caption: "Quality spruce",
+                theme: "light"
             },
             {
                 name: "carving",
+                caption: "Quick Prototyping",
                 theme: "light"
             },
             {
                 name: "contrast",
+                caption: "Bright Spruce GT1080",
                 theme: "dark"
             },
             {
                 name:  "finished-bright",
+                caption: "Bright Spruce M960",
                 theme: "dark"
             },
             {
                 name: "finished-dark",
+                caption: "Gloss finish",
                 theme: "dark"
             },
             {
                 name: "front_plate-bright",
+                caption: "Built with precision",
                 theme: "light"
             },
             {
                 name: "full_darkbg",
+                caption: "Dark spruce T100",
                 theme: "dark"
             },
             {
                 name: "necks",
+                caption: "Choose prebuilt parts",
                 theme: "light"
             },
             {
                 name: "neck-semi",
+                caption: "Fret styles",
                 theme: "light"
             },
             {
                 name: "progress-dark",
+                caption: "Building process",
                 theme: "light"
             },
             {
                 name: "wall_bright",
+                caption: "Cedar M8080",
                 theme: "dark"
             },
             {
                 name: "wood_pallette",
+                caption: "Build your own rosette",
                 theme: "dark"
             },
             {
                 name: "wood-mixed",
+                caption: "Choose your perfect wood combinations",
                 theme: "light"
             },
         ]
@@ -69,6 +82,17 @@ var app = new Vue({
 
         isLeft (index) {
             return index % 2 == 0;
+        },
+
+        onClick (i) {
+            let el =  $(".img-container").eq(i);
+            if (el.hasClass("active")) {
+                el.removeClass("active");
+            }
+            else {
+                $(".img-container").removeClass("active");
+                el.addClass("active");
+            }
         },
 
         mouseOver (i) {
